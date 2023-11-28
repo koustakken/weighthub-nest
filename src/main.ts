ee2626410a6946809e77e0370befc03926eb78eb
Artem Ivanov <ivanov@hostco.ru>
2023-11-28 12:06:19 +0500

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// swagger
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+	// swagger config 
+	const config = new DocumentBuilder()
+		.setTitle('Weight Hub')
+		.build()
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('swagger', app, document);
+	await app.listen(3000);
 }
 bootstrap();
