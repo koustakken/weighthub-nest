094e5d01bf66fec8f657b745fb2236ceaa98c5ef
Artem Ivanov <ivanov@hostco.ru>
2023-11-28 13:32:10 +0500

    prisma connect to postgres

---
+++ src/db/db.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { DbService } from './db.service';
+import { DbController } from './db.controller';
+
+@Module({
+	controllers: [DbController],
+	providers: [DbService],
+	exports: [DbService],
+})
+export class DbModule { }
